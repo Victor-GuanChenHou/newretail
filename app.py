952ebd7a9cfe17a1e7de476fc4935da1f8779808
Victor-GuanChenHou <vfw0327@gmail.com>
2025-03-30 20:58:44 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -103,6 +103,52 @@ def upload_and_process():
         response.headers['Content-Disposition'] = f"attachment; filename*=utf-8''{encoded_filename}"
 
         return response
+@app.route('/upload_ALL', methods=['POST'])
+def upload_ALL():
+    if 'file' not in request.files:
+        return jsonify({"error": "沒有檔案部分！"}), 400
+
+    file = request.files['file']
+
+    if file.filename == '':
+        return jsonify({"error": "沒有選擇檔案！"}), 400
+
+    if file:
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+        file.save(file_path)
+        
+        # ✅ 執行處理函數，產生 Excel
+        processed_file_path = sub.ALL(file_path)
+
+        if processed_file_path.startswith("連線失敗"):
+           return jsonify({"error": processed_file_path}), 500
+
+        filename = file_path
+        encoded_filename = urllib.parse.quote(filename)
+
+        @after_this_request
+        def cleanup(response):
+            def delayed_delete():
+                time.sleep(10)
+                try:
+                    os.remove(processed_file_path)
+                    os.remove(file_path)
+                    print(f"✅ 已刪除檔案: {processed_file_path}")
+                except Exception as e:
+                    print(f"⚠️ 刪除檔案失敗: {e}")
+
+            threading.Thread(target=delayed_delete, daemon=True).start()
+            return response
+
+        response = send_file(
+            processed_file_path,
+            as_attachment=True,
+            download_name=filename,
+            mimetype="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+        )
+        response.headers['Content-Disposition'] = f"attachment; filename*=utf-8''{encoded_filename}"
+
+        return response
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=70, debug=True)
